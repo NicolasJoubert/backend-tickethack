85c9a9217e9a4fa6794a0bbed9f4a8e7b1b26cc2
Mehmet Aladagli <mehmet.aladagli@gmail.com>
2024-11-06 11:51:56 +0100

    new route get for Cart

--- routes/cart.js
+++ routes/cart.js
@@ -3,15 +3,11 @@ var router = express.Router();
 
 const Cart = require("../models/cart.js");
 
-router.delete("/:id", function (req, res, next) {
-  const tripId = req.params.id;
-  Cart.deleteOne({ _id: tripId }).then((result) => {
-    if (result.deletedCount > 0) {
-      res.json({ result: true });
-    } else {
-      res.json({ result: false, error: "City not found" });
-    }
+  router.get("/", (req, res) => {
+    Cart.find().then((data) => {
+      res.json({ allTripsofCart: data });
+    });
   });
-});
+
 
 module.exports = router;
