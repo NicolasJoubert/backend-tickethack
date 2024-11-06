b3271dcd41e34a796bebc1b25659530108eac7ec
NicolasJoubert <nicolasjoubert@icloud.com>
2024-11-06 15:56:34 +0100

    add rod carts

--- routes/cart.js
+++ routes/cart.js
@@ -3,23 +3,45 @@ var router = express.Router();
 
 const Cart = require("../models/carts.js");
 
+const Booking = require("../models/bookings.js");
 
-  router.get("/", (req, res) => {
-    Cart.find().then((data) => {
-      res.json({ allTripsofCart: data });
-    });
+router.get("/", (req, res) => {
+  Cart.find().then((data) => {
+    res.json({ allTripsofCart: data });
   });
+});
 
-  router.delete("/:id", function (req, res, next) {
-
-    const tripId = req.params.id;
-   Cart.deleteOne({ _id: tripId }).then((result) => {
-     if (result.deletedCount > 0) {
-       res.json({ result: true });
+router.delete("/:id", function (req, res, next) {
+  const tripId = req.params.id;
+  Cart.deleteOne({ _id: tripId }).then((result) => {
+    if (result.deletedCount > 0) {
+      res.json({ result: true });
     } else {
       res.json({ result: false, error: "City not found" });
     }
-   });
-   });
+  });
+});
+
+router.post("/purchase", (req, res) => {
+  const { departure, arrival, date, price } = req.body;
+
+  const BookingTrip = new Booking({
+    departure: departure,
+    arrival: arrival,
+    date: date,
+    price: price,
+  });
+
+  BookingTrip.save().then((data) => {
+    if (data) {
+      res.json({
+        result: true,
+        message: "Trip added to booking",
+        trip: data,
+      });
+    } else res.json({ result: false, error: "Error" });
+    console.log(data);
+  });
+});
 
 module.exports = router;
