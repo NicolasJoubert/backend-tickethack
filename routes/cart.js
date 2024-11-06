63fc11b3b1b834e75e31bebcf9446d9031ad230e
Mehmet Aladagli <mehmet.aladagli@gmail.com>
2024-11-06 12:36:37 +0100

    back end cart

--- routes/cart.js
+++ routes/cart.js
@@ -5,10 +5,21 @@ const Cart = require("../models/carts.js");
 
 
   router.get("/", (req, res) => {
-    Cart.findAll().then((data) => {
+    Cart.find().then((data) => {
       res.json({ allTripsofCart: data });
     });
   });
 
+  router.delete("/:id", function (req, res, next) {
+
+    const tripId = req.params.id;
+   Cart.deleteOne({ _id: tripId }).then((result) => {
+     if (result.deletedCount > 0) {
+       res.json({ result: true });
+    } else {
+      res.json({ result: false, error: "City not found" });
+    }
+   });
+   });
 
 module.exports = router;
